d29bf7dc69d80f5bfee22c08fc6e1b36ab3424a3
kirill2121555 <kirill2121555@gmail.com>
2022-08-12 00:31:16 +0300

    fix

--- router/router.js
+++ router/router.js
@@ -3,9 +3,7 @@ const router=new Router()
 const controller=require('../controller/controller')
 
 router.post('/createlink', controller.createlink)
-//router.get('/link/:linkk', controller.redirect)
-router.get('/link', controller.redirect)
-
+router.get('/link/:linkk', controller.redirect)
 router.post('/createcastomlink', controller.createcastomlink)
 router.post('/linkstatistics', controller.statistics)
 
