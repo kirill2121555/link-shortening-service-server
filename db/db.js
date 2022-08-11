c7a8f1918c924f41639dc1125e95d0cce03fd49d
kirill2121555 <kirill2121555@gmail.com>
2022-08-11 17:33:55 +0300

    fix

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
-    password: 'root',
-    host: 'localhost',
+    user: 'gwsccdhssjkjrj',
+    password: 'c152c3cd7f224d0f9326c6df7426385f7f7ad31eff8b4422b48c3a4947da3dd3',
+    host: 'ec2-52-210-97-223.eu-west-1.compute.amazonaws.com',
     port: 5432,
-    database: 'link',
+    database: 'd60p5nsortrthm',
 })
 
 module.exports = pool
